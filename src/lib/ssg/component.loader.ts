8116368974c59eeb5112bfcd351107cfe0bba150
Alexander Holmbäck <alex@ahbk.se>
2025-04-06 21:02:46 +0200

    build: check for non-existing components

--- src/lib/ssg/component.loader.ts
+++ src/lib/ssg/component.loader.ts
@@ -7,9 +7,11 @@ export const componentMap = import.meta.glob('$components/**/*.svelte', {
 }) as ComponentMap;
 
 export const resolveComponent = (content: ComponentContent): ResolvedComponent => {
-  const { component, ...props } = content;
-  return {
-    component: componentMap[`/${config.componentRoot}/${component}.svelte`],
-    props
-  };
+  const { component: name, ...props } = content;
+  const path = `/${config.componentRoot}/${name}.svelte`;
+  const component = componentMap[path];
+
+  if (!component) throw new Error(`Component not found: ${name}`);
+
+  return { component, props };
 };
